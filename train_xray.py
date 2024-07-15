de03e55fa560fe2b8d4f447c94a05fb7b74b497e
chLi <565353780@qq.com>
2024-07-15 11:54:50 +0800

    finish converge with yolov10n

--- train_xray.py
+++ train_xray.py
@@ -3,10 +3,11 @@ sys.path.append('../yolov10')
 
 from ultralytics import YOLOv10
 
-model_yaml_file_path = './yolo_v_ten/Config/Model/yolov10x.yaml'
+model_yaml_file_path = './yolo_v_ten/Config/Model/yolov10n.yaml'
 train_yaml_file_path = './yolo_v_ten/Config/Train/xray.yaml'
 dataset_yaml_file_path = './yolo_v_ten/Config/Dataset/xray.yaml'
 
-model = YOLOv10(model_yaml_file_path, 'detect')
+# model = YOLOv10(model_yaml_file_path, 'detect')
+model = YOLOv10('../yolov10/runs/detect/train2/weights/last.pt')
 
 model.train(cfg=train_yaml_file_path, data=dataset_yaml_file_path)
