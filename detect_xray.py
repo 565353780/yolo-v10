414063b2189ad3262cb8dacb50c8b7d0dcd0824c
chLi <565353780@qq.com>
2024-07-24 19:34:59 +0800

    finish auto save detect results, for further refine model

--- detect_xray.py
+++ detect_xray.py
@@ -4,23 +4,29 @@ sys.path.append('../yolov10')
 import os
 import json
 import torch
-from tqdm import tqdm
+from tqdm import trange
 
 from ultralytics import YOLOv10
 
+model_file_path = '../yolov10/runs/detect/v5-yolov10x-train4k-no_mosiac-no_erase-final/weights/best.pt'
+test_image_folder_path = '/home/chli/Dataset/X-Ray/test1/'
+save_folder_name = 'yolov10x-train4k-thr01'
+conf_threshold = 0.01
+
+save_folder_path = './output/' + save_folder_name
+os.makedirs(save_folder_path + '/', exist_ok=True)
+
+model = YOLOv10(model_file_path, 'detect')
+
+image_filename_list = os.listdir(test_image_folder_path)
+
+image_filename_list.sort()
+
+results_list = []
+
 with torch.no_grad():
-    model_file_path = '../yolov10/runs/detect/train/weights/best.pt'
-    test_image_folder_path = '/home/chli/Dataset/X-Ray/test1/'
-
-    model = YOLOv10(model_file_path, 'detect')
-
-    image_filename_list = os.listdir(test_image_folder_path)
-
-    image_filename_list.sort()
-
-    results_list = []
-
-    for image_filename in tqdm(image_filename_list):
+    for i in trange(len(image_filename_list)):
+        image_filename = image_filename_list[i]
         if image_filename[-4:] != '.jpg':
             continue
 
@@ -28,9 +34,10 @@ with torch.no_grad():
 
         image_file_path = test_image_folder_path + image_filename
 
-        results = model.predict(image_file_path)[0]
+        result = model.predict(image_file_path, conf=conf_threshold)[0]
+        result.save(filename=save_folder_path + '/' + str(i) + '.jpg')
 
-        boxes = results.boxes.data.detach().clone().cpu().numpy()
+        boxes = result.boxes.data.detach().clone().cpu().numpy()
 
         for i in range(8):
             current_label_boxes = boxes[boxes[:, 5].astype(int) == i]
@@ -39,7 +46,5 @@ with torch.no_grad():
 
         results_list.append(current_results)
 
-    os.makedirs('./output/', exist_ok=True)
-
-    with open('./output/yolov10x_train4k_mAP82.json', 'w') as f:
+    with open(save_folder_path + '.json', 'w') as f:
         json.dump(results_list, f)
--- train_xray.py
+++ train_xray.py
@@ -7,7 +7,7 @@ model_yaml_folder_path = './yolo_v_ten/Config/Model/'
 train_yaml_file_path = './yolo_v_ten/Config/Train/xray.yaml'
 dataset_yaml_file_path = './yolo_v_ten/Config/Dataset/xray.yaml'
 
-model = YOLOv10(model_yaml_folder_path + 'xray-v1.yaml', 'detect')
+model = YOLOv10(model_yaml_folder_path + 'xray-v2.yaml', 'detect')
 # model = YOLOv10('../yolov10/runs/detect/v4-yolov10x-train4k-final/weights/best.pt')
 
 model.train(cfg=train_yaml_file_path, data=dataset_yaml_file_path)
